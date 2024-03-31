ffec3cd4ee63b5107fae8a2b7fa9bb6bb475e7aa
fengjutian <2640111081@qq.com>
2024-03-31 16:37:35 +0800

    first commit

---
+++ public/colorpop.js
@@ -0,0 +1,72 @@
+/**
+ * Loads an image into canvas.
+ * Ideally, this function should return Promises to better
+ * handle callbacks but for simplicity sake it's eliminated.
+ */
+function loadImage(src) {
+    const img = new Image();
+    img.crossOrigin = '';
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+  
+    // Load the image on canvas
+    img.addEventListener('load', () => {
+      // Set canvas width, height same as image
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+  
+      pop();
+    });
+  
+    img.src = src;
+  }
+  
+  
+  /**
+   * Applies color pop an image
+   */
+  async function pop() {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    // Loading the model
+    const net = await bodyPix.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      multiplier: 0.75,
+      quantBytes: 2
+    });
+    
+    // Segmentation
+    const { data:map } = await net.segmentPerson(canvas, {
+      internalResolution: 'low',
+    });
+    
+    
+    // Extracting image data
+    const { data:imgData } = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    
+    // Creating new image data
+    const newImg = ctx.createImageData(canvas.width, canvas.height);
+    const newImgData = newImg.data;
+    
+    for(let i=0; i<map.length; i++) {
+      const [r, g, b, a] = [imgData[i*4], imgData[i*4+1], imgData[i*4+2], imgData[i*4+3]];
+  
+      const gray = ((0.3 * r) + (0.59 * g) + (0.11 * b));
+      [
+        newImgData[i*4],
+        newImgData[i*4+1],
+        newImgData[i*4+2],
+        newImgData[i*4+3]
+      ] = !map[i] ? [gray, gray, gray, 255] : [r, g, b, a];
+    }
+    
+    
+    // Draw the new image back to canvas
+    ctx.putImageData(newImg, 0, 0);
+    
+  }
+  
+  loadImage('./sample.jpeg');
